6af9a40d6fbcb426bbdbbf13d565fa5059382692
nbuchanan745 <n.buchanan74.5@gmail.com>
2025-09-12 15:10:19 -0700

    update

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -120,7 +120,10 @@ echo "Library dependencies"
 #${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
-
+pwd 
+echo "${SYSROOT}"
+echo "${OUTDIR}"
+cat ${SYSROOT}/lib/ld-linux-aarch64.so.1
 cp -a ${SYSROOT}/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib
 cp -a ${SYSROOT}/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64
 cp -a ${SYSROOT}/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64
